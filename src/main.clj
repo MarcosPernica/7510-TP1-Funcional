3b3171a703b36331be0ac0fb07fa5b15c2ed2d87
Marcos <marcospernica@gmail.com>
2017-09-04 18:24:50 -0300

    Version entregable

---
+++ src/main.clj
@@ -0,0 +1,11 @@
+(ns main
+  (:require [logical-interpreter :refer :all]
+            [parent-database-test :refer :all]
+            [number-database-test :refer :all]
+            [incomplete-database-test :refer :all]
+            [clojure.test :refer :all]))
+
+
+(run-tests 'incomplete-database-test)
+(run-tests 'parent-database-test)
+(run-tests 'number-database-test)
