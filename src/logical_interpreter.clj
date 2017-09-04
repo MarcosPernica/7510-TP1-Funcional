439aca07f9bfd93cb5787329948520ab55f6e62b
MarcosPernica <marcospernica@gmail.com>
2017-09-04 18:02:58 -0300

    Delete logical_interpreter.clj

--- src/logical_interpreter.clj
+++
@@ -1,7 +0,0 @@
-(ns logical-interpreter)
-
-(defn evaluate-query
-  "Returns true if the rules and facts in database imply query, false if not. If
-  either input can't be parsed, returns nil"
-  [database query]
-  nil)
